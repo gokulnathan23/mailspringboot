826b3b21abeaa8c9de6e732ff803868287a4678f
Gokulsmart23 <gokulsmart23@gmail.com>
2019-12-21 10:35:47 +0530

    mailapp

---
+++ mailapp/src/main/java/com/ustglobal/mailapp/MailappApplication.java
@@ -0,0 +1,14 @@
+package com.ustglobal.mailapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailappApplication.class, args);
+		System.out.println("run");
+	}
+
+}
